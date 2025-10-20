484d1e4cf50c70ff573815876d12868959ee652e
Роман Іванченко <trukfokus22@gmail.com>
2025-10-20 10:03:27 +0300

    Додав обчислення суми чисел у feature-sum

--- TestApp.java
+++ TestApp.java
@@ -11,4 +11,9 @@ public class TestApp {
         System.out.println("GIT test application");
     }
 }
+public static void main(String[] args) {
+    float a = 10;
+    float b = 12.5f;
+    System.out.println("a + b = " + (a + b));
+}
 

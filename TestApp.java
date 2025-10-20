6b88a8499f49a0024b7f1743ad10d6484f3b1d2d
Роман Іванченко <trukfokus22@gmail.com>
2025-10-20 10:06:14 +0300

    Зміни у feature-sum

--- TestApp.java
+++ TestApp.java
@@ -11,9 +11,6 @@ public class TestApp {
         System.out.println("GIT test application");
     }
 }
-public static void main(String[] args) {
-    float a = 10;
-    float b = 12.5f;
-    System.out.println("a + b = " + (a + b));
-}
+System.out.println("Це зміни з гілки feature-sum");
+
 

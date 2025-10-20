7650f180fe8e4fa9b70edd75d54d72b72aa249d3
Роман Іванченко <trukfokus22@gmail.com>
2025-10-20 09:45:57 +0300

    Додав введення чисел через Scanner

--- TestApp.java
+++ TestApp.java
@@ -1,6 +1,14 @@
-public static void main(String[] args) {
-    float a = 10;
-    float b = 12.5f;
-    System.out.println("a + b = " + (a + b));
-    System.out.println("GIT test application");
+import java.util.Scanner;
+
+public class TestApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("1st num: ");
+        double a = Double.valueOf(in.nextLine());
+        System.out.print("2nd num: ");
+        double b = Double.valueOf(in.nextLine());
+        System.out.println("a + b = " + (a + b));
+        System.out.println("GIT test application");
+    }
 }
+
